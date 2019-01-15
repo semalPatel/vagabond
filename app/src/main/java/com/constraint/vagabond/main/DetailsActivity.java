1ed214089f0741b870ea7ed55d81066e4b24aff9
semal259 <semalpatel2596@gmail.com>
2019-01-15 14:57:28 -0800

    add detailed activity screen

--- app/src/main/java/com/constraint/vagabond/main/DetailsActivity.java
+++ app/src/main/java/com/constraint/vagabond/main/DetailsActivity.java
@@ -5,35 +5,52 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.constraint.vagabond.R;
+import com.constraint.vagabond.data.DataStore;
+import com.constraint.vagabond.data.RecreationalAreaList;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity implements DetailsContract {
 
-  CollapsingToolbarLayout collapsingToolbarLayout;
-  Toolbar toolbar;
-  FloatingActionButton favButton;
-  String imageUrl;
+  private CollapsingToolbarLayout collapsingToolbarLayout;
+  private Toolbar toolbar;
+  private FloatingActionButton favButton;
+  private String collapsingImageUrl;
+  private TextView areaDescription;
+  private RecreationalAreaList detailedArea = DataStore.getRecreationalAreaList();
+  private List<String> imageUrls;
+  private int position;
+  private RecyclerView areaImages;
+  private Button directionBtn;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Intent getImage = getIntent();
-    imageUrl = getImage.getStringExtra("image_url");
+    Intent getData = getIntent();
+    collapsingImageUrl = getData.getStringExtra("image_url");
+    position = getData.getIntExtra("position", 0);
+    imageUrls = detailedArea.recreationalAreaList.get(position).getImageUrls();
     initializeView();
   }
 
   @Override
   protected void onStart() {
     super.onStart();
+    initializeData();
+    loadImage();
+    setDataToRecyclerView();
   }
 
   @Override
@@ -43,21 +60,16 @@ public class DetailsActivity extends AppCompatActivity implements DetailsContrac
     setSupportActionBar(toolbar);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     collapsingToolbarLayout = findViewById(R.id.collapsing_layout);
-    collapsingToolbarLayout.setTitle("Yosemite");
-    loadImage();
+    areaDescription = findViewById(R.id.description);
     favButton = findViewById(R.id.fav_button);
-    favButton.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            Snackbar.make(v, "Added to watchlist", Snackbar.LENGTH_LONG);
-          }
-        });
+    areaImages = findViewById(R.id.horizontal_recycler_view);
+    areaDescription = findViewById(R.id.description);
+    directionBtn = findViewById(R.id.directions);
   }
 
   public void loadImage() {
     final ImageView expandingImage = findViewById(R.id.collapsing_image);
-    Picasso.get().load(imageUrl).fit().centerCrop().into(expandingImage);
+    Picasso.get().load(collapsingImageUrl).fit().centerCrop().into(expandingImage);
   }
 
   @Override
@@ -71,8 +83,17 @@ public class DetailsActivity extends AppCompatActivity implements DetailsContrac
   public void destroyView() {}
 
   @Override
-  public void initializeData() {}
+  public void initializeData() {
+    collapsingToolbarLayout.setTitle(detailedArea.recreationalAreaList.get(position).recAreaName);
+    String desc = detailedArea.recreationalAreaList.get(position).recAreaDescription;
+    areaDescription.setText(desc);
+  }
 
   @Override
-  public void setDataToRecyclerView() {}
+  public void setDataToRecyclerView() {
+    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+    areaImages.setLayoutManager(linearLayoutManager);
+    DetailsPhotosAdapter detailsPhotosAdapter = new DetailsPhotosAdapter(imageUrls);
+    areaImages.setAdapter(detailsPhotosAdapter);
+  }
 }
--- app/src/main/java/com/constraint/vagabond/main/DetailsPhotosAdapter.java
+++ app/src/main/java/com/constraint/vagabond/main/DetailsPhotosAdapter.java
@@ -1,4 +1,65 @@
 package com.constraint.vagabond.main;
 
-public class DetailsPhotosAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.constraint.vagabond.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.Random;
+
+public class DetailsPhotosAdapter extends RecyclerView.Adapter<DetailsPhotosAdapter.Images> {
+
+  private List<String> imagesUrls;
+
+  public DetailsPhotosAdapter(List<String> imageUrls) {
+    this.imagesUrls = imageUrls;
+  }
+
+  @NonNull
+  @Override
+  public Images onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+    View view = layoutInflater.inflate(R.layout.horizontal_images_grid, viewGroup, false);
+    return new Images(view);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Images images, int i) {
+    String randomUrl = imagesUrls.get(images.getAdapterPosition());
+    Picasso.get().load(randomUrl).fit().centerCrop().into(images.image);
+  }
+
+  @Override
+  public int getItemCount() {
+    return imagesUrls.size();
+  }
+
+  class Images extends RecyclerView.ViewHolder {
+
+    final ImageView image;
+
+      Images(@NonNull View itemView) {
+      super(itemView);
+      image = itemView.findViewById(R.id.more_images);
+      imageOnClickListener();
+    }
+
+    void imageOnClickListener(){
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+  }
+
+
 }
