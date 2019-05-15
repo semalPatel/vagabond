fde99de1c654ac86b44277fcbd9e5a7db2cc6f20
semal259 <semalpatel2596@gmail.com>
2019-05-15 14:15:18 -0700

    minor typo

--- app/src/main/java/com/constraint/vagabond/data/entities/RecreationalAreaList.kt
+++ app/src/main/java/com/constraint/vagabond/data/entities/RecreationalAreaList.kt
@@ -5,4 +5,4 @@ import com.google.gson.annotations.SerializedName
 
 data class RecreationalAreaList(@field:SerializedName("RECDATA")
                            @field:Expose
-                           val recreationalAreaList: List<RecreationalArea>)g
+                           val recreationalAreaList: List<RecreationalArea>)
