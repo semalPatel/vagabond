d5e07a1abf6880c22895ad56e3bf3832aea67171
semal259 <semalpatel2596@gmail.com>
2019-05-16 01:14:45 -0700

    remove unnecessary safe calls assertions

--- app/src/main/java/com/constraint/vagabond/main/search/MainActivity.kt
+++ app/src/main/java/com/constraint/vagabond/main/search/MainActivity.kt
@@ -19,15 +19,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainContract.MainView, SearchView.OnQueryTextListener {
 
-    private var progressBar: ProgressBar? = null
-    private var presenter: MainContract.Presenter? = null
+    private lateinit var presenter: MainContract.Presenter
     private var errorSnackBar: Snackbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initializeToolbarAndRecyclerView()
-        initProgressBar()
         presenter = MainPresenterImpl(this)
     }
 
@@ -35,27 +33,15 @@ class MainActivity : AppCompatActivity(), MainContract.MainView, SearchView.OnQu
         setSupportActionBar(main_app_toolbar)
         main_app_toolbar.setTitle(R.string.app_name)
         val layoutManager = LinearLayoutManager(this@MainActivity)
-        recyclerViewID!!.layoutManager = layoutManager
-    }
-
-    private fun initProgressBar() {
-        progressBar = ProgressBar(this, null, android.R.attr.progressBarStyleLarge)
-        progressBar!!.isIndeterminate = true
-        val relativeLayout = RelativeLayout(this)
-        relativeLayout.gravity = Gravity.CENTER
-        relativeLayout.addView(progressBar)
-        val params = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT)
-        progressBar!!.visibility = View.INVISIBLE
-        this.addContentView(relativeLayout, params)
+        recyclerViewID.layoutManager = layoutManager
     }
 
     override fun showProgress() {
-        progressBar!!.visibility = View.VISIBLE
+        progress_bar.visibility = View.VISIBLE
     }
 
     override fun hideProgress() {
-        progressBar!!.visibility = View.INVISIBLE
+        progress_bar.visibility = View.INVISIBLE
     }
 
     override fun onResponseFailure() {
@@ -71,7 +57,7 @@ class MainActivity : AppCompatActivity(), MainContract.MainView, SearchView.OnQu
 
     override fun onDestroy() {
         super.onDestroy()
-        presenter!!.onDestroy()
+        presenter.onDestroy()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -79,16 +65,20 @@ class MainActivity : AppCompatActivity(), MainContract.MainView, SearchView.OnQu
         val menuItem = menu.findItem(R.id.app_bar_search)
         val searchView = menuItem.actionView as SearchView
         searchView.maxWidth = android.R.attr.maxWidth
+        searchView.queryHint = getString(R.string.national_park_hint)
         searchView.setOnQueryTextListener(this)
         return true
     }
 
     override fun onQueryTextSubmit(s: String): Boolean {
-        presenter!!.onSearch(s, getString(R.string.api_key))
+        presenter.onSearch(s, getString(R.string.api_key))
         return false
     }
 
     override fun onQueryTextChange(s: String): Boolean {
         return false
     }
+
+
+
 }
